c6989ef90097ba916fc6f02b39fd1cddb98d233e
Simon Lindsay <simonlindsay@gmail.com>
2018-11-30 17:02:28 -0800

    adjusted header

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,30 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import { withTheme } from 'emotion-theming'
+import { Box, Flex } from '@rebass/grid'
 import Logo from './logo'
 import ButtonCTA from './button-cta'
 import ButtonMenu from './button-menu'
 
-const Header = styled('header')`
+const Header = styled(Flex)`
   background-color: ${props => props.theme.bgColor2};
   width: 100%;
-  height: 90px;
   box-shadow: 0 0 20px rgba(75, 0, 255, 0.44);
 `
 
-const HeaderContent = styled('div')`
-  display: flex;
-  flex-direction: row;
-  flex-wrap: nowrap;
-  justify-content: space-between;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-  max-width: 1200px;
-  margin: 0 auto;
-  padding: 1rem;
-`
-
 export default withTheme(props => (
-  <Header>
-    <HeaderContent>
-      <div className="header__content__logo">
-        <Logo />
-      </div>
+  <Header as="header" alignItems="center" justifyContent="center">
+    <Flex
+      alignItems="center"
+      justifyContent="space-between"
+      maxWidth="1200"
+      width="100%"
+      p={3}
+      css={{
+        maxWidth: '1200px'
+      }}
+    >
+      <Logo />
       <div>
         <ButtonMenu href="#">Why Pledge</ButtonMenu>
         <ButtonMenu href="#">Our Partners</ButtonMenu>
@@ -41,6 +35,6 @@ export default withTheme(props => (
           Join us today
         </ButtonCTA>
       </div>
-    </HeaderContent>
+    </Flex>
   </Header>
 ))
