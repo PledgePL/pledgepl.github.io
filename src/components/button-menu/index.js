ae6aadc37ea0713c5a167080ba07744a8f4888f0
Simon Lindsay <simonlindsay@gmail.com>
2018-12-24 01:47:06 -0800

    button menu active  state

--- src/components/button-menu/index.js
+++ src/components/button-menu/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from '@emotion/styled'
+import { Match } from '@reach/router'
 import { withTheme } from 'emotion-theming'
 import { themeGet } from 'styled-system'
 import Text from '../text'
@@ -19,11 +20,13 @@ const ButtonMenu = styled(CoreBox)`
   &:after {
     content: '';
     position: absolute;
-    background-color: ${props => themeGet(`colors.beta`, '#fff')(props)};
+    background-color: ${props =>
+      themeGet(props.active ? 'colors.delta' : 'colors.beta', '#fff')(props)};
     transform-origin: center;
-    transform: scaleX(0);
+    transform: ${props => `scaleX(${props.active ? 1 : 0})`};
     transition: transform 0.2s cubic-bezier(0.47, 0, 0.745, 0.715) 0s,
-      background-color 0.2s linear 0.3s;
+      background-color 0.2s linear 0s;
+    transition-delay: ${props => (props.active ? '0s, 0s' : '0s, 0.2s')};
     ${props => {
       return mq({
         height: [3, 5],
@@ -38,35 +41,36 @@ const ButtonMenu = styled(CoreBox)`
       })
     }};
   }
-  &[aria-current]:after,
   &:hover:after {
     transform: scaleX(1);
-  }
-  &[aria-current]:after {
-    transition-delay: 0s, 0s;
-    background-color: ${props => themeGet(`colors.delta`, '')(props)};
   }
 `
 
 const LinkButtonMenu = ButtonMenu.withComponent(Link)
 
-export default React.memo(
-  withTheme(({ theme, href, children }) => {
-    return (
-      <LinkButtonMenu href={href} p={[1, 2]}>
-        <Text
-          inline={true}
-          color="beta"
-          fontFamily="beta"
-          textColor="textColor2"
-          textSize="eta"
-          textAlign="center"
-          whiteSpace="nowrap"
-          letterSpacing={0.5}
-        >
-          {children}
-        </Text>
-      </LinkButtonMenu>
-    )
-  })
-)
+export default ({ href, children }) => {
+  const isActive = ({ isPartiallyCurrent }) => {
+    return isPartiallyCurrent ? { className: 'active' } : null
+  }
+
+  return (
+    <Match path={href}>
+      {props => (
+        <LinkButtonMenu href={href} p={[1, 2]} active={props.match}>
+          <Text
+            inline={true}
+            color="beta"
+            fontFamily="beta"
+            textColor="textColor2"
+            textSize="eta"
+            textAlign="center"
+            whiteSpace="nowrap"
+            letterSpacing={0.5}
+          >
+            {children}
+          </Text>
+        </LinkButtonMenu>
+      )}
+    </Match>
+  )
+}
