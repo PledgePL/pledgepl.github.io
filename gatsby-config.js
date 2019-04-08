75921fc20f86235488a393910616e0561847d39e
Simon Lindsay <simonlindsay@gmail.com>
2019-04-07 22:55:30 -0700

    refactored manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -67,18 +67,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'pledge-pl',
-        short_name: 'ppl',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/pledgepl-logo-purple.png', // This path is relative to the root of the site.
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
